8e5c515d65036e7232b11f1d171fe612f37df073
Shoaib Khan <mdshoaib.a.khan@gmail.com>
2023-07-17 23:18:11 +0600

    commited

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
         Gauss_Elimination gaussElimination = new Gauss_Elimination();
         Least_Square_Straight_Line leastSquareStraightLine = new Least_Square_Straight_Line();
         Bisection_Method bisectionMethod = new Bisection_Method();
+        False_Position falsePosition = new False_Position();
+        Newton_Raphson_Method newtonRaphsonMethod = new Newton_Raphson_Method();
         Secant_Method secantMethod = new Secant_Method();
-        secantMethod.solve();
+        newtonRaphsonMethod.solve();
 
     }
 }
--- src/Newton_Raphson_Method.java
+++ src/Newton_Raphson_Method.java
@@ -2,12 +2,7 @@ public class Newton_Raphson_Method {
         public static void solve() {
             double x1=-1*Math.random()*100, x2;
 
-            //System.out.println(x1 + " " + x2);
 
-//        if(equation.eq(x1)*equation.eq(x1)>0){
-//            System.out.println("No root");
-//            return;
-//        }
             x2 = x1 + equation(x1)/ diff_equation(x1);
             double tolerance=0.05;
             while(Math.abs(equation(x1)-0)>tolerance){
